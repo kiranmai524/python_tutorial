47c1b4c71ada8818f88f8cae7ef166fc19d997f8
Kiranmai Nallajarla <kiranmai.nallajarla@gmail.com>
2012-04-16 00:14:36 +0530

    new twitter api

---
+++ twitter.py
@@ -0,0 +1,8 @@
+from sys import argv
+import twitter
+script,name = argv
+user = twitter.Api()
+tweets_5 = user.GetUserTimeline(name)
+print " latest 5 tweets from Channel %r:"%name
+for i in range(0,6,1):
+		print " %r\t %s\n"%(i,tweets_5[i].text)
